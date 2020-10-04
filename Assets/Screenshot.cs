f4e4e98433b580e9f5011775a408c36c65d1d884
Sam <sambaqvel@gmail.com>
2020-10-04 12:48:33 +0200

    Foliage, animations and particles

---
+++ Assets/Screenshot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    [SerializeField]
+    int _upscaling = 1;
+
+    int _ss;
+
+    void Start()
+    {
+        _ss = 0;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            ScreenCapture.CaptureScreenshot("screenshot" + _ss + ".png", _upscaling);
+            _ss++;
+
+            print("Screenshot number " + _ss + " has been captured.");
+        }
+    }
+}
